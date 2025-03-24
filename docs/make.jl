344f0d5e8e35321d3ebf2b23ec5ad0b1e34fc4bd
martinkosch <martin.kosch@gmail.com>
2025-03-24 10:47:28 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,9 +8,11 @@ makedocs(;
     authors="Martin Kosch <martin.kosch@gmail.com> and contributors",
     sitename="NonlinearCrystals.jl",
     format=Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
         canonical="https://martinkosch.github.io/NonlinearCrystals.jl",
         edit_link="main",
         assets=String[],
+        repolink = "https://github.com/martinkosch/NonlinearCrystals.jl"
     ),
     pages=[
         "Home" => "index.md",
